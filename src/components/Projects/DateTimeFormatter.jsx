072af2b5d4909d86ab344af37014b3bf4503d2f1
witold <wiltold.wroblewski@sennder.com>
2021-01-27 18:15:54 +0100

    fix locale date format

--- src/components/Projects/DateTimeFormatter.jsx
+++ src/components/Projects/DateTimeFormatter.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const DateTimeFormatter = ({ dateTime }) => {
-  const formattedDate = new Intl.DateTimeFormat('fr-Fr', {
+  const formattedDate = new Intl.DateTimeFormat('fr-FR', {
     dateStyle: 'full',
     timeStyle: 'short',
   }).format(new Date(Date.parse(dateTime)));
